a1779309d958fa1d772135f2372ec4ada88c4694
nameerakhter <akhtarnameer@gmail.com>
2024-07-20 18:53:41 +0530

    CustomFormFeild and enums

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
 
-const PatientForm = () => {
-  return (
-    <div>PatientForm</div>
-  )
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import CustomFormFeild from "../CustomFormFeild";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT='phoneInput',
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datepicker",
+  SELECT= "select",
+  SKELETON = "skeleton",
 }
 
-export default PatientForm
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const PatientForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there... </h1>{" "}
+        </section>
+        <p className="text-dark-700">Schedule your first appointment</p>
+
+        <CustomFormFeild
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name='name'
+          label='Full Name'
+          placeholder='Enter your full name'
+          iconSrc='/assets/icons/user.svg'
+          iconAlt='user icon'
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
