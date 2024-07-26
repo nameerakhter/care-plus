d8c3246dbcc78e9aded98dfd78bddbd6b9e39c00
nameerakhter <akhtarnameer@gmail.com>
2024-07-26 14:06:56 +0530

    Passkey validation modal UI

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState } from "react";
 import {
   AlertDialog,
@@ -11,27 +11,75 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const PasskeyModal = () => {
-    const [open, setopen] = useState(false)
+  const router = useRouter();
+  const [open, setOpen] = useState(true);
+  const [passkey, setPasskey] = useState('')
+  const [error, setError] = useState('')
+
+  const validatePasskey = ( e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    if(passkey ===process.env.NEXT_PUBLIC_PASSKEY){
+
+  }else{
+    setError('Invalid Passkey, Please try again')
+  }
+}
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
-      <AlertDialogContent>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+          </AlertDialogTitle>
+          <Image
+            src="/assets/icons/close.svg"
+            alt="close"
+            width={20}
+            height={20}
+            onClick={() => closeModal()}
+            className="cursor-pointer"
+          />
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            To Access the admin page please access the passkey!
           </AlertDialogDescription>
         </AlertDialogHeader>
+        <div>
+          <InputOTP maxLength={6} value={passkey} onChange={(value)=> setPasskey(value)}>
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          {error && <p className="shad-error text-14-regular mt-4 flex justify-center">{error}</p>}
+        </div>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={(e)=> validatePasskey(e)} className="shad-primary-btn w-full">Enter the Admin Passkey</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
   );
 };
+
 
 export default PasskeyModal;
