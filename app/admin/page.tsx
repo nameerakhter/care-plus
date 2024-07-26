fc0d4d7a907c91c746495550df8d09ce2ac6c5f3
nameerakhter <akhtarnameer@gmail.com>
2024-07-26 16:24:27 +0530

    started with StatCards

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,38 @@
+import StatCard from '@/components/StatCard'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <header className='admin-header'>
+            <Link href='cursor-pointer'>
+            <div className="flex items-start">
+            <Image
+              src="/assets/icons/Logomark.svg"
+              height={32}
+              width={162}
+              alt="logo"
+              className="h-8 w-fit"
+            ></Image>
+            <p className="text-2xl">CarePlus+</p>
+          </div></Link>
+
+          <p className='text-16-semibold'>Admin Dashboard</p>
+        </header>
+        <div className="admin-main">
+            <section className='w-full space-y-4'>
+                <h1 className='header'>Welcome...</h1>
+                <p className='text-dark-700'>Start the day by managing new appointments</p>
+            </section>
+            <section className='admin-stat'>
+                <StatCard type="appointments" count={5} label="Schedule Appointmetns" icon='/assets/icons/appointments.svg' />
+                <StatCard type="pending" count={10} label="Pending Appointmetns" icon='/assets/icons/pending.svg' />
+                <StatCard type="cancelled" count={9} label="Cancelled Appointmetns" icon='/assets/icons/cancelled.svg' />
+            </section>
+        </div>
+    </div>
   )
 }
 
