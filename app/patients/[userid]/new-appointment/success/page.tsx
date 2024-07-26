18d09afa7d1abeba3976d08635a45a896be8b649
nameerakhter <akhtarnameer@gmail.com>
2024-07-26 12:41:00 +0530

    appointmentId typo fix

--- app/patients/[userid]/new-appointment/success/page.tsx
+++ app/patients/[userid]/new-appointment/success/page.tsx
@@ -1,8 +1,15 @@
+import { Doctors } from "@/constants";
+import { getAppointments } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Success = () => {
+const Success = async ({params:{userId}, searchParams}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || ""
+  const appointment = await getAppointments(appointmentId);
+  const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician);
+
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -27,7 +34,8 @@ const Success = () => {
           <section className="request-details">
             <p>Requested Appointment details</p>
             <div className="flex items-center gap-3">
-              {/* <Image></Image> */}
+              <Image
+              src={doctor?.image!} alt={doctor?.name!} width={100} height={100} className="size-6"></Image>
             </div>
           </section>
       </div>
