9422ab8c4fdbafebd7a201192dfb068ca02b1a59
nameerakhter <akhtarnameer@gmail.com>
2025-02-26 20:12:28 +0530

    fix: Build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import PatientForm from "@/components/forms/PatientForm";
-import PasskeyModal from "@/components/PasskeyModal";
-import Image from "next/image";
-import Link from "next/link";
+import PatientForm from '@/components/forms/PatientForm'
+import PasskeyModal from '@/components/PasskeyModal'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Home({searchParams}: SearchParamProps) {
-
-  const isAdmin = searchParams?.admin === "true";
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === 'true'
   return (
-    
     <div className="flex h-screen max-h-screen">
-        {isAdmin && <PasskeyModal />}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -20,7 +18,7 @@ export default function Home({searchParams}: SearchParamProps) {
               width={1000}
               alt="logo"
               className="mb-12 h-10 w-fit"
-            ></Image>
+              />
             <p className="text-2xl">CarePlus+</p>
           </div>
           <PatientForm />
@@ -28,11 +26,19 @@ export default function Home({searchParams}: SearchParamProps) {
             <p className="justify-items-end text-dark-600 xl:text-left">
               ©2024 care-plus
             </p>
-            <Link href='/?admin=true' className="text-green-500">Admin</Link>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
         </div>
       </section>
-      <Image src='/assets/images/onboarding-img.png' height={1000} width={1000} alt="onboarding" className="side-img max-w-[50%] rounded-[1vw]"></Image>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding"
+        className="side-img max-w-[50%] rounded-[1vw]"
+        />
     </div>
-  );
+  )
 }
