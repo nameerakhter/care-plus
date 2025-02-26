2e5ecf5e8cb71416e684157116d1db17ec45f0a2
nameerakhter <akhtarnameer@gmail.com>
2025-02-26 20:08:20 +0530

    fix: helper functions

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,8 +1,7 @@
-import * as sdk from "node-appwrite";
-import { config } from "dotenv";
+import * as sdk from 'node-appwrite'
+import { config } from 'dotenv'
 
-// Load environment variables from .env file
-config();
+config()
 
 export const {
   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
@@ -13,18 +12,16 @@ export const {
   DOCTOR_COLLECTION_ID,
   APPOINTMENT_COLLECTION_ID,
   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-} = process.env;
-// console.log(`Appwrite Configuration:`);
-// console.log(`ENDPOINT: ${ENDPOINT}`);
-// console.log(`PROJECT_ID: ${PROJECT_ID}`);
-// console.log(`API_KEY: ${API_KEY}`);
+} = process.env
 
+const client = new sdk.Client()
 
-const client = new sdk.Client();
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID!)
+  .setKey(process.env.NEXT_PUBLIC_API_KEY!)
 
-client.setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!).setProject(process.env.NEXT_PUBLIC_PROJECT_ID!).setKey(process.env.NEXT_PUBLIC_API_KEY!);
-
-export const databases = new sdk.Databases(client);
-export const users = new sdk.Users(client);
-export const messaging = new sdk.Messaging(client);
-export const storage = new sdk.Storage(client);
+export const databases = new sdk.Databases(client)
+export const users = new sdk.Users(client)
+export const messaging = new sdk.Messaging(client)
+export const storage = new sdk.Storage(client)
