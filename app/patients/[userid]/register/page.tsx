cf1822e1ebec91181f4a5ed93ed7cab8667bc08d
nameerakhter <akhtarnameer@gmail.com>
2025-02-26 20:11:48 +0530

    fix: linting errors

--- app/patients/[userid]/register/page.tsx
+++ app/patients/[userid]/register/page.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
 import React from 'react'
 import * as Sentry from '@sentry/nextjs'
 
-const Register = async ({params:{userId}}: SearchParamProps) => {
-  const user =  await getUser(userId)
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId)
 
-  Sentry.metrics.set("user_view_register", user.name);
+  Sentry.metrics.set('user_view_register', user.name)
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex flex-col py-10">
+        <div className="sub-container flex max-w-[860px] flex-col py-10">
           <div className="flex items-start">
             <Image
               src="/assets/icons/Logomark.svg"
@@ -20,16 +20,20 @@ const Register = async ({params:{userId}}: SearchParamProps) => {
               width={1000}
               alt="logo"
               className="mb-12 h-10 w-fit"
-            ></Image>
+              />
             <p className="text-2xl">CarePlus+</p>
           </div>
-          <RegisterForm user={user}/>
-            <p className="copyright py-12">
-              ©2024 care-plus
-            </p>
+          <RegisterForm user={user} />
+          <p className="copyright py-12">©2024 care-plus</p>
         </div>
       </section>
-        <Image src='/assets/images/register-img.png' height={1000} width={1000} alt="onboarding" className="side-img max-w-[390px] rounded-[1vw]"></Image>
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding"
+        className="side-img max-w-[390px] rounded-[1vw]"
+        />
     </div>
   )
 }
