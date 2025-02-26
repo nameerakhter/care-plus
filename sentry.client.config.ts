dd97db7ecb7ea437cf239c954cdd81e200d69814
nameerakhter <akhtarnameer@gmail.com>
2025-02-26 20:09:19 +0530

    fix: prettier errors

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://ca8e62ddd87f6674b0203a20325c566f@o4507684563386368.ingest.de.sentry.io/4507684566073424",
+  dsn: 'https://ca8e62ddd87f6674b0203a20325c566f@o4507684563386368.ingest.de.sentry.io/4507684566073424',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -27,4 +27,4 @@ Sentry.init({
       blockAllMedia: true,
     }),
   ],
-});
+})
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://ca8e62ddd87f6674b0203a20325c566f@o4507684563386368.ingest.de.sentry.io/4507684566073424",
+  dsn: 'https://ca8e62ddd87f6674b0203a20325c566f@o4507684563386368.ingest.de.sentry.io/4507684566073424',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
