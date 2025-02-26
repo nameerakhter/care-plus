738f049008d2c41665ec858a0168b715ddb1f38c
nameerakhter <akhtarnameer@gmail.com>
2025-02-26 20:11:34 +0530

    fix: NextImage tags syntax + add senty metrics

--- app/patients/[userid]/new-appointment/page.tsx
+++ app/patients/[userid]/new-appointment/page.tsx
@@ -1,18 +1,20 @@
-import AppointmentForm from "@/components/forms/AppointmentForm";
-import { Button } from "@/components/ui/button";
-import { getPatient } from "@/lib/actions/patient.actions";
-import Image from "next/image";
-import Link from "next/link";
+import AppointmentForm from '@/components/forms/AppointmentForm'
+import { Button } from '@/components/ui/button'
+import { getPatient } from '@/lib/actions/patient.actions'
+import Image from 'next/image'
+import Link from 'next/link'
 import * as Sentry from '@sentry/nextjs'
 
-export default async function NewAppointment({params: {userId}}: SearchParamProps) {
-  const patient = await getPatient(userId);
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId)
 
-  Sentry.metrics.set("user_view_new-appointment", patient.name);
+  Sentry.metrics.set('user_view_new-appointment', patient.name)
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex flex-col py-10">
+        <div className="sub-container flex max-w-[860px] flex-col py-10">
           <div className="flex items-start">
             <Image
               src="/assets/icons/Logomark.svg"
@@ -20,19 +22,24 @@ export default async function NewAppointment({params: {userId}}: SearchParamProp
               width={1000}
               alt="logo"
               className="mb-12 h-10 w-fit"
-            ></Image>
+              />
             <p className="text-2xl">CarePlus+</p>
           </div>
-          <AppointmentForm 
-          type='create'
-          userId={userId}
-          patientId={patient.$id}/>
-            <p className="copyright py-12">
-              ©2024 care-plus
-            </p>
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+          <p className="copyright py-12">©2024 care-plus</p>
         </div>
       </section>
-        <Image src='/assets/images/appointment-img.png' height={1000} width={1000} alt="onboarding" className="side-img max-w-[390px] rounded-[1vw]"></Image>
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding"
+        className="side-img max-w-[390px] rounded-[1vw]"
+        />
     </div>
-  );
+  )
 }
